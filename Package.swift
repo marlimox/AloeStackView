af59703fe4ad02a84310401d6646178bc59ee9ef
Florian Ludot <9365651+florianldt@users.noreply.github.com>
2019-04-08 01:28:58 +0900

    swift 5 support (#46)
    replace index(of:) by firstIndex(of:) to quiet the warning on Swift 5

--- Package.swift
+++ Package.swift
@@ -1,9 +1,12 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 
 import PackageDescription
 
 let package = Package(
     name: "AloeStackView",
+    platforms: [
+       .ios(.v9),
+    ],
     products: [
         .library(
             name: "AloeStackView", 
