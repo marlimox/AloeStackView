f22162c22fa67291462f33903c2a7198041489f5
AloeStackView Admin <49097724+aloestackview-admin@users.noreply.github.com>
2019-07-07 14:33:28 +0100

    Add support for horizontal stack views.
    This change adds the ability to change the `axis` of an
    AloeStackView between vertical and horizontal directions. The
    `axis` can be changed dynamically, and separators become vertical
    when the `axis` is `.horizontal`.
    
    This change combines and builds on the work of PRs #15 and #23, and
    addresses issue #13.
    
    Special thanks to @neobeppe and @apptekstudios for their proposals
    for implementing this feature.

--- Example/AloeStackViewExample/ViewControllers/MainViewController.swift
+++ Example/AloeStackViewExample/ViewControllers/MainViewController.swift
@@ -42,6 +42,7 @@ public class MainViewController: AloeStackViewController {
     setUpSwitchRow()
     setUpHiddenRows()
     setUpExpandingRowView()
+    setUpHorizontalRow()
     setUpPhotoRow()
   }
 
@@ -93,6 +94,77 @@ public class MainViewController: AloeStackViewController {
   private func setUpExpandingRowView() {
     let expandingRow = ExpandingRowView()
     stackView.addRow(expandingRow)
+  }
+
+  private func setUpHorizontalRow() {
+    let titleLabel = UILabel()
+    titleLabel.font = UIFont.preferredFont(forTextStyle: .body)
+    titleLabel.numberOfLines = 0
+    titleLabel.text = "Use a horizontal layout"
+    stackView.addRow(titleLabel)
+    stackView.hideSeparator(forRow: titleLabel)
+    stackView.setInset(forRow: titleLabel, inset: UIEdgeInsets(
+      top: stackView.rowInset.top,
+      left: stackView.rowInset.left,
+      bottom: 4,
+      right: stackView.rowInset.right))
+
+    let captionLabel = UILabel()
+    captionLabel.font = UIFont.preferredFont(forTextStyle: .caption2)
+    captionLabel.textColor = .blue
+    captionLabel.numberOfLines = 0
+    captionLabel.text = "(Try scrolling horizontally!)"
+    stackView.addRow(captionLabel)
+    stackView.hideSeparator(forRow: captionLabel)
+    stackView.setInset(forRow: captionLabel, inset: UIEdgeInsets(
+      top: 0,
+      left: stackView.rowInset.left,
+      bottom: stackView.rowInset.bottom,
+      right: stackView.rowInset.right))
+
+    let horizontalStackView = AloeStackView()
+    horizontalStackView.axis = .horizontal
+    horizontalStackView.hidesSeparatorsByDefault = true
+    horizontalStackView.showsHorizontalScrollIndicator = false
+
+    horizontalStackView.contentInset = UIEdgeInsets(
+      top: 0,
+      left: stackView.rowInset.left / 2,
+      bottom: 0,
+      right: stackView.rowInset.right / 2)
+
+    horizontalStackView.rowInset = UIEdgeInsets(
+      top: stackView.rowInset.top,
+      left: stackView.rowInset.left / 2,
+      bottom: stackView.rowInset.bottom,
+      right: stackView.rowInset.right / 2)
+
+    horizontalStackView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+
+    stackView.addRow(horizontalStackView)
+    stackView.setInset(forRow: horizontalStackView, inset: .zero)
+
+    guard let image = UIImage(named: "lobster-dog") else { return }
+
+    for imageNumber in 1...10 {
+      let imageView = UIImageView(image: image)
+      imageView.isUserInteractionEnabled = true
+      imageView.contentMode = .scaleAspectFit
+
+      let aspectRatio = image.size.height / image.size.width
+      imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: aspectRatio).isActive = true
+
+      horizontalStackView.addRow(imageView)
+
+      horizontalStackView.setTapHandler(forRow: imageView) { [weak self] _ in
+        let alert = UIAlertController(
+          title: "Tapped on image \(imageNumber)",
+          message: nil,
+          preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .default))
+        self?.present(alert, animated: true)
+      }
+    }
   }
 
   private func setUpPhotoRow() {
