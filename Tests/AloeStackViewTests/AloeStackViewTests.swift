9a515f18a1cd2eb936dd516c99cc94560e82b0cb
Tyler Hedrick <tyler.hedrick@airbnb.com>
2020-06-15 15:06:30 -0700

    Prepare repo for transfer of ownership to @marlimox (#80)
    * Prepare repo for ownership transfer
    
    * Update TODOs
    
    * Update TODOs
    
    * update copyright

--- Tests/AloeStackViewTests/AloeStackViewTests.swift
+++ Tests/AloeStackViewTests/AloeStackViewTests.swift
@@ -1,4 +1,4 @@
-// Copyright 2018 Airbnb, Inc.
+// Copyright Marli Oshlack 2018.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
